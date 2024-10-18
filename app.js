8bc616aee7ed935feeb4ed2562fc58f10e72af13
FAll <940313262@qq.com>
2024-10-18 16:29:29 +0800

    feat: 完善配置

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-// 引入express模块
 const express = require('express');
 const fs = require('fs');
 
-// 创建一个express应用
 const app = express();
-
 app.all('*', function (_req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
@@ -12,18 +9,25 @@ app.all('*', function (_req, res, next) {
   res.header('Content-Type', 'application/json;charset=utf-8');
   next();
 });
-
-// 解析JSON格式的请求体
 app.use(express.json());
 
-// 设置一个GET请求，响应一个简单的消息
-app.get('/get', (_req, res) => {
+
+
+app.get('/programs', (_req, res) => {
     const ret = fs.readFileSync('data.json','utf8');
-    
-    res.status(200).send(ret)
+    res.status(200).send(ret);
 });
 
-// 设置一个POST请求，接受JSON数据并返回
+app.get('/get',(_req,res)=>{
+  const dataJson = fs.readFileSync('data.json','utf8');
+  const data = JSON.parse(dataJson);
+
+  const currentProgram = data["programs"]
+  .filter(e=>e.name === data["active"]) [0]
+  
+  res.status(200).send(currentProgram);
+})
+
 app.post('/set', (req, res) => {
   const data = req.body; 
 
@@ -41,4 +45,3 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
